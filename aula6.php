443d9c2eb729ba6743125fab17db87d0bcdb3581
BJBC <bcaldasbc@gmail.com>
2021-02-15 22:53:01 -0300

    outro

--- aula6.php
+++ aula6.php
@@ -3,7 +3,7 @@
 $pessoas = array();
 
 array_push($pessoas, array(
-	'nome' => 'joao',
+	'nome' => 'joão',
 	'idade' => 20,
 	'sexo' => 'm'
 ));
@@ -21,5 +21,17 @@ echo "<br/>";echo "<br/>";
 print_r($pessoas[1]['sexo']);
 echo "<br/>";echo "<br/>";
 
+echo json_encode($pessoas);
+
+//ESTÁ EM UTF8 -> tudo ok com acentos
+$json = '[{"nome":"jo\u00e3o","idade":20,"sexo":"m"},{"nome":"maria","idade":25,"sexo":"f"}]';
+
+echo "<br/>";echo "<br/>";
+echo "$json";
+
+echo "<br/>";echo "<br/>";
+$data = json_decode($json, true);
+//se não colocar "true" será um objeto
+print_r($data);//para imprimir array
 
 ?>
